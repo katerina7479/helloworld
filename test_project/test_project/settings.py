b48c3091ca7613d34bc66c61d2e39a58a3622bb5
katerina <katerina.h@socialcode.com>
2015-03-21 17:19:17 -0700

    Moved to separate

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -48,9 +48,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'helloworld.urls'
+ROOT_URLCONF = 'test_project.test_project.urls'
 
-WSGI_APPLICATION = 'helloworld.wsgi.application'
+WSGI_APPLICATION = 'test_project.wsgi.application'
 
 
 # Database
--- test_project/test_project/urls.py
+++ test_project/test_project/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'helloworld.views.home', name='home'),
+    # url(r'^$', 'test_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
